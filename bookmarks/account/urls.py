95a531afcf2753ca5784a38a94a8f8b1642ec2c4
Evgeniy Malikov <malikov.evgeniy@gmail.com>
2023-10-09 00:35:30 +0300

    аунтификация

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_vievs
 
-from . import views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    # path("login/", views.user_login, name="login"),
+    path("login/", auth_vievs.LoginView.as_view(), name="login"),
+    path("logout/", auth_vievs.LogoutView.as_view(), name="logout"),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -12,16 +12,14 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(
-                request,
-                username=cd["имя пользователя"],
-                password=cd["password"],
+                request, username=cd["username"], password=cd["password"]
             )
             if user is not None:
                 if user.is_active:
                     login(request, user)
                     return HttpResponse("Аутентификация прошла успешно")
                 else:
-                    return HttpResponse("(Отключенная учетная запись")
+                    return HttpResponse("Отключенная учетная запись")
             else:
                 return HttpResponse("Недопустимый логин")
     else:
