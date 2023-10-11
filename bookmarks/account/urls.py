6352374bdde5687e88d73e06e7aaaca473bfde4a
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-10-11 17:24:11 +0300

    user

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_vievs
 
+from . import views
+
 
 urlpatterns = [
     # path("login/", views.user_login, name="login"),
     path("login/", auth_vievs.LoginView.as_view(), name="login"),
     path("logout/", auth_vievs.LogoutView.as_view(), name="logout"),
+    path("", views.dashboard, name="dashboard"),
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -90,3 +90,7 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
