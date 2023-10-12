08cd6e0a1c8ffa242dc190a2715c9ae0c63885b7
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-10-12 13:29:48 +0300

    форма смены пароля

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-
 from .forms import LoginForm
 
 
