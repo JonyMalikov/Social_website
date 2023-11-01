28de47b10cbe13da77a777e694deae067660987d
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-11-01 15:36:53 +0300

    register

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -6,3 +7,23 @@ class LoginForm(forms.Form):
 
     username = forms.CharField(label="Имя пользователя")
     password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
+
+
+class UserRegistrationForm(forms.ModelForm):
+    """форма регистрации пользователя"""
+
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Повторите пароль", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "email"]
+
+    def clean_password2(self):
+        """валидация подтверждения пароля"""
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("Пароли не совпадают")
+        return cd["password2"]
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -38,4 +38,5 @@ urlpatterns = [
     # ),
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
