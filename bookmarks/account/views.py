81b60f4635178f0a3896cfe12437d448b44a8599
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-11-02 15:23:05 +0300

    исправление ошибок

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm
 
 
@@ -34,15 +33,25 @@ def user_login(request):
     return render(request, "account/login.html", {"form": form})
 
 
+# def register(request):
+#     """регистрация нового пользователя"""
+#     if request.method == "POST":
+#         new_user = user_form.save(commit=False)
+#         new_user.set_password(user_form.cleaned_data["password"])
+#         new_user.save()
+#         return render(request, "account/register_done.html", {"new_user": new_user})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, "account/register.html", {"user_form": user_form})
 def register(request):
     """регистрация нового пользователя"""
     if request.method == "POST":
-        new_user = user_form.save(commit=False)
-        new_user.set_password(user_form.cleaned_data["password"])
-        new_user.save()
-        return render(
-            request, "account/register_done.html", {"new_user": new_user}
-        )
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, "account/register.html", {"user_form": user_form})
