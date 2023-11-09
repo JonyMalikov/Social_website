9f76d66b684227ac019a340bc835e7860d4cd9aa
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-11-10 00:35:10 +0300

    message

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -5,9 +5,22 @@ from django.db import models
 class Profile(models.Model):
     """Профиль пользователя"""
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="пользователь",
+    )
+    date_of_birth = models.DateField(
+        blank=True, null=True, verbose_name="дата рождения"
+    )
+    photo = models.ImageField(
+        upload_to="users/%Y/%m/%d/", blank=True, verbose_name="фото"
+    )
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["user__username"]
