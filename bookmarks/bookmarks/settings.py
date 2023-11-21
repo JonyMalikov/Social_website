e3d9e00fbe3f5fb8c108e0ee03e441349ee1fa06
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2023-11-21 21:52:29 +0300

    соцаальная аунтификация

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = (
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 INSTALLED_APPS = [
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
 ]
 
 MIDDLEWARE = [
